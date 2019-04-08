7be8ff05dc7348e25577001490cb82860932326b
ModishSanchit <ModishSanchit@gmail.com>
2019-04-08 19:55:11 +0530

    hunter2

---
+++ hunter2.py
@@ -0,0 +1,10 @@
+num=int(input())
+lis=list(map(int,input().split()))
+res=set()
+for i in l:
+    if l.count(i)>1:
+       res.add(i)
+    else:
+        print("unique")
+for i in s:
+    print(str(i)+" ",end="")
