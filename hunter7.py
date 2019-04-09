689154e15efcc590f16201f4fa388110f1deddf3
ModishSanchit <ModishSanchit@gmail.com>
2019-04-09 21:09:51 +0530

    hunter question 7 uploaded

---
+++ hunter7.py
@@ -0,0 +1,11 @@
+num = int(input())
+lis = list(map(int,input().split()))
+res_list=[]
+for i in range(0,len(lis)):
+    if(lis[i]%2 == 0 and i % 2 !=0):
+        res_list.append(lis[i])
+    if(lis[i]%2 != 0 and i % 2 == 0):
+        res_list.append(lis[i])
+
+for i in res_list:
+    print(res_list,end="")
